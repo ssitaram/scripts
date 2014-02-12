74922c880d89b4b5ece2ef82b251b945cd33f1a8
Sunayana Sitaram <sunayana.sitaram@gmail.com>
2014-02-12 18:24:56 -0500

    fix for Egyptian, missing phone in mapping table

--- lexicons/LexConvert.py
+++ lexicons/LexConvert.py
@@ -1,5 +1,7 @@
 #LDC - Festival format or LDC - Sphinx format
-#also add a buckwalter option
+#option to either read from a mapping file or convert to buckwalter
+#option to convert to festival format or sphinx format
+#option to generate phoneset file
 
 LEX = open("../data/ar_lex.utf8")
 lex = LEX.read()
@@ -35,18 +37,29 @@ for line in lex.split("\n")[:-1]:
 	native_word = line.split("\t")[NATIVE_FIELD-1]
 	pron_word_mult = line.split("\t")[PRON_FIELD-1]
 	if pron_word_mult.find("/") != -1:
-		print("mult words")
-		pron_word = pron_word_mult
+		#for cases when the lexicon has two or more pronunciations for the word
+		#print("mult words")
+		pron_word = pron_word_mult.split("//")
 	else:
-		pron_word = pron_word_mult
-	print(native_word+" "+pron_word)
-	phoneme_string = ""
-	for letter in pron_word:
-		if letter != "ay" or letter != "aw":
-			mapped_letter = ph_map[letter]
-			phoneme_string += " "+mapped_letter
-	print(phoneme_string)
-
+		pron_word = [pron_word_mult]
+	for pw in pron_word:
+		#print(native_word+" "+pw)
+		phoneme_string = ""
+		for letter in pw:
+			if letter != "ay" or letter != "aw":
+				try:
+					mapped_letter = ph_map[letter]
+					phoneme_string += " "+mapped_letter
+				except KeyError:
+					print("Key not found for "+letter)
+					#fix for Egyptian, may be wrong
+					if letter == "B":
+						mapped_letter = "B"
+						phoneme_string += " "+mapped_letter
+		#print(phoneme_string)
+		OUTF = open("festival_lex", "a")
+		OUTF.write(native_word+" "+phoneme_string[1:]+"\n")
+		OUTF.close()
 OUTS = open("sphinx_dic", "w")		
 OUTS.close()
 
